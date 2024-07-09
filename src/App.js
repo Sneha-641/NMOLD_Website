a7a04672df9194dabf0e5121fc8522111d443e4a
sneha <snehagoyal214@gmail.com>
2024-07-09 12:54:41 +0530

    Form added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-// import Home from './pages/Home';
-// import WhyChooseUs from './pages/WhyChooseUs';
-// import Delivers from './pages/Delivers';
-// import ClientReview from './pages/ClientReview';
-// import Pricing from './pages/Pricing';
-// import ContactUs from './pages/ContactUs';
-// import BannerBottom from './pages/BannerBottom';
-// import { Footer } from './pages/Footer';
-// import CopyrightBanner from './pages/CopyrightBanner';
+import Home from './pages/Home';
+import WhyChooseUs from './pages/WhyChooseUs';
+import Delivers from './pages/Delivers';
+import ClientReview from './pages/ClientReview';
+import Pricing from './pages/Pricing';
+import ContactUs from './pages/ContactUs';
+import BannerBottom from './pages/BannerBottom';
+import { Footer } from './pages/Footer';
+import CopyrightBanner from './pages/CopyrightBanner';
 import Form from './pages/Form';
 // https://www.figma.com/design/OWcieRQi0UaZrbx5sQM9jJ/NMOLD?node-id=0-1&t=Fz6quNMjalRuuomr-0
 function App() {
